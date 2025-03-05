4837524dbc6ec004d2b71148c2e582241063c255
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2025-03-05 21:12:13 +0100

    added update_image macro

--- src/main.rs
+++ src/main.rs
@@ -12,36 +12,46 @@ slint::include_modules!();
 const MIN_DELAY: Duration = Duration::from_millis(600);
 
 /// Syntactic sugar for async in slint callback
-/// 
-/// ### Params 
+///
+/// ### Params
 /// 1) existing App Window variable
 /// 2) name of ui handle to be used for ui calls inside async
-/// 3) existing Mutex App Logic variable 
-/// 4) name of the locked logic name to be used for logic calls inside async 
+/// 3) existing Mutex App Logic variable
+/// 4) name of the locked logic name to be used for logic calls inside async
 /// 5) all the code to be placed in async block (isolated by brackets)
 macro_rules! async_context {
     ($last_date:ident, $ui:ident, $logic:ident, $code:block) => {{
-        let ui_handle:slint::Weak<AppWindow> = $ui.as_weak();
+        let ui_handle: slint::Weak<AppWindow> = $ui.as_weak();
         let logic_ref: Arc<Mutex<AppLogic>> = $logic.clone();
-        move |repeat:bool| {
+        move |repeat: bool| {
             if repeat {
-                if Instant::now()- $last_date < MIN_DELAY {
-                    return 
+                if Instant::now() - $last_date < MIN_DELAY {
+                    return;
                 }
                 $last_date = Instant::now();
             }
             let logic_c = logic_ref.clone();
             #[allow(unused)]
             let $ui = ui_handle.unwrap();
-            slint::spawn_local(async_compat::Compat::new(async move { 
+            slint::spawn_local(async_compat::Compat::new(async move {
                 #[allow(unused_mut)]
                 let mut $logic = logic_c.lock().await;
-                $code 
-            })).unwrap();
+                $code
+            }))
+            .unwrap();
         }
-    }}
+    }};
 }
 
+macro_rules! update_image {
+    ($ui:ident, $logic:ident) => {{
+        let img: ImageStat = $logic.get_img().await;
+        $ui.set_photo_path(img.image);
+        $ui.set_photo_num(img.number as i32);
+        $ui.set_total_num(img.out_of as i32);
+        $ui.set_photo_name(img.name.into());
+    }};
+}
 
 // no #[tokio::main] because it crashes after a few Mutex locks (compatibility issue with slint)
 fn main() -> Result<(), Box<dyn Error>> {
@@ -50,58 +60,49 @@ fn main() -> Result<(), Box<dyn Error>> {
     // TODO PARAM
     // let folder_path = PathBuf::from_str("/home/guilhem/Pictures/TEST").unwrap();
     let folder_path = PathBuf::from_str("/home/guilhem/Documents/SAVE/Photos/2024_20 - Palawan").unwrap();
-    let logic = Arc::new(Mutex::new(AppLogic::new(folder_path))); 
+    let logic = Arc::new(Mutex::new(AppLogic::new(folder_path)));
     let first: ImageStat = logic.blocking_lock().get_first_img();
     ui.set_total_num(first.out_of as i32);
     ui.set_photo_num(first.number as i32);
     ui.set_photo_name(first.name.into());
     ui.set_photo_path(first.image);
     let mut last_cmd = Instant::now();
-    
+
     let logic_c = logic.clone();
     slint::spawn_local(async_compat::Compat::new(async move {
         let mut logic = logic_c.lock().await;
         logic.init().await;
-    })).unwrap();
+    }))
+    .unwrap();
 
-   
-    ui.on_next(async_context!{last_cmd, ui, logic, {
+    ui.on_next(async_context! {last_cmd, ui, logic, {
         if logic.next_img().await{
-            let img: ImageStat = logic.get_img().await;
-            ui.set_photo_path(img.image);
-            ui.set_photo_num(img.number as i32);
-            ui.set_photo_name(img.name.into());
+            update_image!(ui, logic);
         }
     }});
 
-    ui.on_prev(async_context!{last_cmd, ui, logic, {
+    ui.on_prev(async_context! {last_cmd, ui, logic, {
         if logic.prev_img().await {
-            let img: ImageStat = logic.get_img().await;
-            ui.set_photo_path(img.image);
-            ui.set_photo_num(img.number as i32);
-            ui.set_photo_name(img.name.into());
+            update_image!(ui, logic);
         }
     }});
 
-
-    ui.on_edit(async_context!{last_cmd, ui, logic, {
+    ui.on_edit(async_context! {last_cmd, ui, logic, {
         logic.edit();
-    }}); 
-    
-    ui.on_delete(async_context!{last_cmd, ui, logic, {
+    }});
+
+    ui.on_delete(async_context! {last_cmd, ui, logic, {
         if logic.delete().await {
-            let img: ImageStat = logic.get_img().await;
-            ui.set_photo_path(img.image);
-            ui.set_photo_num(img.number as i32);
-            ui.set_total_num(img.out_of as i32);
-            ui.set_photo_name(img.name.into());
+            update_image!(ui, logic);
         }
         else {
             slint::quit_event_loop().unwrap();
         }
     }});
 
-    ui.on_close(|| {slint::quit_event_loop().unwrap();});
+    ui.on_close(|| {
+        slint::quit_event_loop().unwrap();
+    });
 
     ui.run()?;
 
