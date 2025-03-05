6a35e2670e70523831e43abe20338ef7208185a2
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2025-03-05 14:24:14 +0100

    clean and update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::sync::Mutex;
 
-use pgt::logic::AppLogic;
+use pgt::logic::{AppLogic, ImageStat};
 
 slint::include_modules!();
 
@@ -20,19 +20,20 @@ const MIN_DELAY: Duration = Duration::from_millis(100);
 /// 4) name of the locked logic name to be used for logic calls inside async 
 /// 5) all the code to be placed in async block (isolated by brackets)
 macro_rules! async_context {
-    ($last_date:ident, $ui:ident, $ui_clone:ident, $logic_arc:ident, $logic_locked:ident, $code:block) => {{
+    ($last_date:ident, $ui:ident, $logic:ident, $code:block) => {{
         let ui_handle:slint::Weak<AppWindow> = $ui.as_weak();
-        let logic_ref: Arc<Mutex<AppLogic>> = $logic_arc.clone();
+        let logic_ref: Arc<Mutex<AppLogic>> = $logic.clone();
         move || {
             if Instant::now()- $last_date < MIN_DELAY {
                 return 
             }
             $last_date = Instant::now();
             let logic_c = logic_ref.clone();
-            let $ui_clone = ui_handle.unwrap();
+            #[allow(unused)]
+            let $ui = ui_handle.unwrap();
             slint::spawn_local(async_compat::Compat::new(async move { 
                 #[allow(unused_mut)]
-                let mut $logic_locked = logic_c.lock().await;
+                let mut $logic = logic_c.lock().await;
                 $code 
             })).unwrap();
         }
@@ -40,8 +41,7 @@ macro_rules! async_context {
 }
 
 
-
-// #[tokio::main]
+// no #[tokio::main] because it crashes after a few Mutex locks (compatibility issue with slint)
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
     ui.window().set_maximized(true);
@@ -49,59 +49,51 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let folder_path = PathBuf::from_str("/home/guilhem/Pictures/TEST").unwrap();
     let folder_path = PathBuf::from_str("/home/guilhem/Documents/SAVE/Photos/2024_20 - Palawan").unwrap();
     let logic = Arc::new(Mutex::new(AppLogic::new(folder_path))); 
-    ui.set_photo_path(logic.blocking_lock().get_first_img());
-    let (photo_name, photo_num, total_num) = logic.blocking_lock().get_img_infos();
-    ui.set_total_num(total_num as i32);
-    ui.set_photo_name(photo_name.into());
-    ui.set_photo_num(photo_num as i32);
+    let first: ImageStat = logic.blocking_lock().get_first_img();
+    ui.set_total_num(first.out_of as i32);
+    ui.set_photo_num(first.number as i32);
+    ui.set_photo_name(first.name.into());
+    ui.set_photo_path(first.image);
     let mut last_cmd = Instant::now();
 
     
     let logic_c = logic.clone();
     slint::spawn_local(async_compat::Compat::new(async move {
-        let mut locked_logic = logic_c.lock().await;
-        locked_logic.init();
+        let mut logic = logic_c.lock().await;
+        logic.init();
     })).unwrap();
 
    
-    ui.on_next(async_context!{last_cmd, ui, ui_c, logic, locked_logic, {
-        if locked_logic.next_img().await{
-            ui_c.set_photo_path(locked_logic.get_img().await);
-            let (photo_name, photo_num , _total_num) = locked_logic.get_img_infos();
-            ui_c.set_photo_num(photo_num as i32);
-            ui_c.set_photo_name(photo_name.into());
-            //TODO Check if faster to do rotation ? 
-            // ui_c.set_photo_rotation(90);
+    ui.on_next(async_context!{last_cmd, ui, logic, {
+        if logic.next_img().await{
+            let img: ImageStat = logic.get_img().await;
+            ui.set_photo_path(img.image);
+            ui.set_photo_num(img.number as i32);
+            ui.set_photo_name(img.name.into());
         }
     }});
 
-    ui.on_prev(async_context!{last_cmd, ui, ui_c, logic, locked_logic, {
-        if locked_logic.prev_img().await {
-            ui_c.set_photo_path(locked_logic.get_img().await);
-            let (photo_name, photo_num , _total_num) = locked_logic.get_img_infos();
-            ui_c.set_photo_num(photo_num as i32);
-            ui_c.set_photo_name(photo_name.into());
+    ui.on_prev(async_context!{last_cmd, ui, logic, {
+        if logic.prev_img().await {
+            let img: ImageStat = logic.get_img().await;
+            ui.set_photo_path(img.image);
+            ui.set_photo_num(img.number as i32);
+            ui.set_photo_name(img.name.into());
         }
     }});
 
 
-    ui.on_edit(async_context!{last_cmd, ui, _ui_c, logic, locked_logic, {
-        locked_logic.edit();
+    ui.on_edit(async_context!{last_cmd, ui, logic, {
+        logic.edit();
     }}); 
-    ui.on_edit({
-        let logic_ref = logic.clone();
-        move || {
-            // let logic_c = logic_ref.clone(); // Clone the Rc
-            logic_ref.blocking_lock().edit();
-        }
-    });
-    ui.on_delete(async_context!{last_cmd, ui, ui_c, logic, locked_logic, {
-        if locked_logic.delete().await {
-            ui_c.set_photo_path(locked_logic.get_img().await);
-            let (photo_name, photo_num , total_num) = locked_logic.get_img_infos();
-            ui_c.set_photo_num(photo_num as i32);
-            ui_c.set_total_num(total_num as i32);
-            ui_c.set_photo_name(photo_name.into());
+    
+    ui.on_delete(async_context!{last_cmd, ui, logic, {
+        if logic.delete().await {
+            let img: ImageStat = logic.get_img().await;
+            ui.set_photo_path(img.image);
+            ui.set_photo_num(img.number as i32);
+            ui.set_total_num(img.out_of as i32);
+            ui.set_photo_name(img.name.into());
         }
         else {
             slint::quit_event_loop().unwrap();
